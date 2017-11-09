a3af852e0d69a0668fe78b993e8aa1cdb75d80e4
Polonius <benjamin.fellows@gmail.com>
2017-11-09 00:35:12 -0400

    Cleaned up build folder and merged creation into build script.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # simple-calc-java
 Simple calculator built in Java. Trying to remember how to do this stuff. Suggestions welcome.
 
-Run the build script to create and run the app.jar file.
+`bash ./build.sh` to create and run the app.jar file.
+
+`bash ./run.sh` if you want to run it again.
--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+# Creates ./build/classes if it doesn't exist;
+# cleans up any existing app.jar file;
+# builds all .java files under ./src into ./build/classes;
+# creates the app.jar file;
+# runs the app.jar file.
+
+if [ ! -d build ]; then
+  mkdir build && mkdir build/classes
+elif [ ! -d build/classes ]; then
+  mkdir build/classes
+fi
+
 rm -rf app.jar &&
 javac -d build/classes $(find src -name *.java) &&
 jar cfe app.jar com.polonius.java.Program -C build/classes . &&
 java -jar app.jar
+
